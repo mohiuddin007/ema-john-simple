f56216a040a3261d4f39fd6c4acc06e8d457b3f2
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-10-01 15:55:43 +0600

    ema jhon with server

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 
 const ProductDetail = () => {
     const {productKey} = useParams();
-    const product = fakeData.find(pd => pd.key === productKey);
+    const [product, setProduct] = useState({});
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/product/' + productKey)
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    }, [productKey])
+   // const product = fakeData.find(pd => pd.key === productKey);
     return (
         <div>
             <h1>Your product detail</h1>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import fakeData from '../../fakeData';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
@@ -7,20 +6,28 @@ import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseMana
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
-
-    const frist10 = fakeData.slice(0,10);
-    const [products, setProducts] = useState(frist10);
+    // const frist10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+
+    useEffect(()=> {
+        fetch('http://localhost:4000/products')
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data)
+        })
+    },[])
 
     useEffect(() =>{
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
-        const previousCart = productKeys.map( existingKey => {
-            const product = fakeData.find(pd => pd.key === existingKey)
-            product.quantity = savedCart[existingKey];
-            return product;
-        })
-        setCart(previousCart);
+        fetch('http://localhost:4000/productsByKeys', {
+           method: 'POST', 
+           headers: { 'Content-Type': 'application/json'},
+           body: JSON.stringify(productKeys)
+       })
+       .then(res => res.json())
+       .then(data => setCart(data))
     },[])
 
     const handleAddProduct = (product) =>{
